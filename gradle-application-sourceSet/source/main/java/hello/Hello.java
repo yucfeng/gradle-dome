7fe23eb704e9115379161c516045ae94fbe662d7
yucfeng <lxfyc314@163.com>
2017-12-22 11:36:27 +0800

    generate application

--- gradle-application-sourceSet/source/main/java/hello/Hello.java
+++ gradle-application-sourceSet/source/main/java/hello/Hello.java
@@ -6,8 +6,8 @@ public class Hello {
 
     public static void main(String[] args) {
         DateTime dateTime2000Year = new DateTime(2000, 2, 29, 0, 0, 0);
-        System.out.println(dateTime2000Year); // out: 2000-02-29T00:00:00.000+08:00
-        DateTime dateTime1997Year = dateTime2000Year.withYear(1997);
-        System.out.println(dateTime1997Year); // 1997-02-28T00:00:00.000-08:00
+        System.out.println(dateTime2000Year); // 2000-02-29T00:00:00.000+08:00
+        DateTime dateTimeYear = dateTime2000Year.withYear(Integer.parseInt(args[0]));
+        System.out.println(dateTimeYear);
     }
 }
